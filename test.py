ec4dea0c40d8cb9d75eb7bab91204a3ebec72d2f
MohammedBakr44 <mohammed.mobile.tricks@gmail.com>
2020-06-19 09:23:56 +0200

    Changing the main branch

--- test.py
+++
@@ -1,9 +0,0 @@
-import sched, time
-s = sched.scheduler(time.time, time.sleep)
-def do_something(sc): 
-    print("Doing stuff...")
-    # do your stuff
-    s.enter(10, 1, do_something, (sc,))
-
-s.enter(10, 1, do_something, (s,))
-s.run()
